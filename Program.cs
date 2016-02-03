1bbf1c5164456b5f186f3f220bd1ea5abb8f9ff7
VM <mogolivskiy@gmail.com>
2016-02-03 20:31:30 +0200

    Do .Net Lab1

---
+++ Program.cs
@@ -0,0 +1,64 @@
+using System;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic; 
+using System.Linq;
+
+namespace Lab1Mogolivskogo
+{
+	class MainClass
+	{
+		private static List<Student> students = new List<Student>();
+		private delegate bool Comparator<T>(T st1, T st2);
+
+		public static void Main (string[] args)
+		{
+			students.Add(new Student("Ivanov"));
+			students[0].addGrade (".NET", 100);
+			students[0].addGrade ("JavaSE", 81);
+			students[0].addGrade ("JavaEE", 76);
+			students.Add(new Student("Semenov"));
+			students[1].addGrade (".NET", 78);
+			students[1].addGrade ("JavaSE", 61);
+			students[1].addGrade ("JavaEE", 72);
+			students.Add(new Student("Green"));
+			students[2].addGrade (".NET", 88);
+			students[2].addGrade ("JavaSE", 60);
+			students[2].addGrade ("JavaEE", 90);
+			Console.WriteLine("=============STUDENTS=============");
+			showStudents(ref students);
+			Console.WriteLine("=========Ascending sorting========");
+			sort<Student>(ref students, (t1, t2) => t1.getAverageGrade().CompareTo(t2.getAverageGrade()) > 0);
+			showStudents(ref students);
+			Console.WriteLine("========Descending sorting========");
+			sort<Student>(ref students, (t1, t2) => t1.getAverageGrade().CompareTo(t2.getAverageGrade()) < 0);
+			showStudents(ref students);
+		}
+
+		private static void sort<T>(ref List<T> list, Comparator<T> comparator)
+		{
+			T temp;
+			for (int i = 0; i < list.Count; i++)
+			{
+				for (int j = 0; j < list.Count-1; j++)
+				{
+					if (comparator (list [j], list [j + 1]))
+					{
+						temp = list [j];
+						list [j] = list [j + 1];
+						list [j + 1] = temp;
+					}
+				}
+			}
+		}
+
+		private static void showStudents(ref List<Student> students)
+		{
+			Console.WriteLine ("=============STUDENTS=============");
+			foreach(Student s in students)
+				Console.WriteLine(s);
+			Console.WriteLine ("==================================");
+		}
+	}
+	
+}
---
+++ Student.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Text;
+using System.Collections.Generic; 
+namespace Lab1Mogolivskogo
+{
+	class Student
+	{
+		private string _surname;
+		private Dictionary<string, ushort> _grades = new Dictionary<string, ushort>();
+		private float _averageGrade;
+		private bool _wasChanged;
+
+		public Student(String surname)
+		{
+			_surname = surname;
+			_averageGrade = 0;
+			_wasChanged = false;
+		}
+
+		public void addGrade(string subject, ushort grade)
+		{
+			_grades [subject] = grade;
+			_wasChanged = true;
+			Console.WriteLine(_grades[subject] + " added to grades of "+_surname); 
+		}
+
+		public float getAverageGrade()
+		{
+			if(_wasChanged) countAverage();
+			return _averageGrade;
+		}
+
+		private void countAverage()
+		{
+			_averageGrade = 0;
+			foreach (string key in _grades.Keys)
+			{
+				_averageGrade += _grades[key];
+			}
+			_averageGrade /= _grades.Keys.Count;
+		}
+
+		public override string ToString ()
+		{
+			StringBuilder result = new StringBuilder();
+			result.AppendLine ("Grades of Ivanox: ");
+			foreach (string key in _grades.Keys)
+			{
+				result.AppendLine (key + " \t\t "+_grades[key]);
+			}
+			result.AppendLine ("Average Grade \t " + Math.Round(getAverageGrade(),2));
+			return result.ToString();
+		}
+
+	}
+}
+
